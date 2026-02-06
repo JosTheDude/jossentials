438bdf0a096dfb9ab6549b052c502d12475c543c
JosTheDude <jos@joscodes.xyz>
2026-02-06 10:08:35 -0500

    init: initial commit

---
+++ src/main/java/gg/jos/jossentials/Jossentials.java
@@ -0,0 +1,17 @@
+package gg.jos.jossentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jossentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
